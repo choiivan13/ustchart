b1c2e8f8d79b2bf9edc962a8eb6568e29187324a
choiivan13 <wmchoiaa@connect.ust.hk>
2023-01-31 14:39:50 +0800

    feat: db connection and update

--- backend/internal/db/db.go
+++ backend/internal/db/db.go
@@ -1,5 +1,70 @@
 package db
 
-type DbHandler interface {
-	Update() (bool, error)
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/choiivan13/ustchart/backend/types"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+type Operations interface {
+	UpdateSection(s types.Section)
+}
+
+type MongoDBHandler struct {
+	Client *mongo.Client
+}
+
+// !!!
+func NewDBHandler() *MongoDBHandler {
+	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
+	clientOptions := options.Client().
+		ApplyURI(os.Getenv("USTCHARTURL")).
+		SetServerAPIOptions(serverAPIOptions)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+
+	return &MongoDBHandler{
+		Client: client,
+	}
+}
+
+func (m MongoDBHandler) UpdateSection(s types.Section) {
+	collection := m.Client.Database("ustchart").Collection(s.Offering)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	filter := bson.M{
+		"offering":    s.Offering,
+		"coursename":  s.CourseName,
+		"sectionname": s.SectionName,
+	}
+	update := bson.M{
+		"$push": bson.M{"data": s.Data},
+		"$setOnInsert": bson.M{
+			"coursetime":  s.CourseTime,
+			"instructors": s.Instructors,
+		},
+	}
+	upsert := true
+	opt := options.UpdateOptions{
+		Upsert: &upsert,
+	}
+	_, err := collection.UpdateOne(ctx, filter, update, &opt)
+	if err != nil {
+		panic(err)
+	}
 }
--- backend/main/main.go
+++ backend/main/main.go
@@ -8,9 +8,12 @@ import (
 )
 
 func main() {
+	// s := os.Getenv("USTCHARTURL")
+	// fmt.Println(s)
 	mongodb := db.NewDBHandler()
 	c := scraper.NewScraper("2230", 123, mongodb)
 	c.Scrape(time.Now().Unix())
+
 	// s := types.Section{
 	// 	Offering:    "2230",
 	// 	CourseName:  "TEST 1000",
